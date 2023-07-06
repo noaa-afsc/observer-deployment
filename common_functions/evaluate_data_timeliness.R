71128dbd0f1d35509045ce5f3382728818fb8474
Phil.Ganz <phil.ganz@noaa.gov>
2023-07-06 13:16:04 -0800

    change back to measuring data timeliness in mean days

--- common_functions/evaluate_data_timeliness.R
+++ common_functions/evaluate_data_timeliness.R
@@ -1,11 +1,9 @@
 evaluate_data_timeliness <- function(data, strata_timeliness){
   
-  dt <- rbind(merge(data[STRATA != "ZERO"], strata_timeliness, by = c("COVERAGE_TYPE", "STRATA")), 
-              merge(data[STRATA == "ZERO"], strata_timeliness, by.x = c("COVERAGE_TYPE", "AGENCY_GEAR_CODE"), by.y = c("COVERAGE_TYPE", "STRATA"), all.x = TRUE))
+  dt <- rbind(merge(data[STRATA != "ZERO" & AGENCY_GEAR_CODE != "JIG"], strata_timeliness, by = c("COVERAGE_TYPE", "STRATA")), 
+              merge(data[STRATA == "ZERO" & AGENCY_GEAR_CODE != "JIG"], strata_timeliness, by.x = c("COVERAGE_TYPE", "AGENCY_GEAR_CODE"), by.y = c("COVERAGE_TYPE", "STRATA"), all.x = TRUE))
   
-  dt[AGENCY_GEAR_CODE == "JIG", one_week_quantile := 1]
+  dt <- unique(dt[, .(TRIP_ID, mean_data_timeliness)])
   
-  dt <- unique(dt[, .(TRIP_ID, one_week_quantile)])
-  
-  return(round(sum(dt[['one_week_quantile']]) / nrow(dt), 4))  
+  return(round(mean(dt[['mean_data_timeliness']]), 2))  
 }
