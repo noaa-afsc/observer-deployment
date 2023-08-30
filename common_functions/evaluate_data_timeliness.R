e1c217b5db08beb68d88ffcd9e21294c53c51a0c
Phil.Ganz <phil.ganz@noaa.gov>
2023-08-30 09:43:05 -0800

    replace fgem_n_old object with 285, the number of fixed gear EM trips used to calculate data timeliness

--- common_functions/evaluate_data_timeliness.R
+++ common_functions/evaluate_data_timeliness.R
@@ -1,7 +1,7 @@
 evaluate_data_timeliness <- function(data, strata_timeliness, budget, rates, stratification, allocation){
   
   fgem_n_new        <- sum(rates[[paste(stratification, allocation, sep = ".")]][ADP == unique(data$ADP) & BUDGET == budget & STRATUM_COL %in% c("EM_HAL", "EM_POT", "EM_FIXED-BSAI", "EM_FIXED-GOA", "EM_HAL-BSAI", "EM_HAL-GOA", "EM_POT-BSAI", "EM_POT-GOA"), n])
-  strata_timeliness <- strata_timeliness[COVERAGE_TYPE == "PARTIAL"][STRATA %in% c("EM_HAL", "EM_POT"), mean_data_timeliness := round(mean_data_timeliness * (fgem_n_new / fgem_n_old), 2)]
+  strata_timeliness <- strata_timeliness[COVERAGE_TYPE == "PARTIAL"][STRATA %in% c("EM_HAL", "EM_POT"), mean_data_timeliness := round(mean_data_timeliness * (fgem_n_new / 285), 2)]
   
   dt <- rbind(merge(data[STRATA != "ZERO" & AGENCY_GEAR_CODE != "JIG"], strata_timeliness, by = c("COVERAGE_TYPE", "STRATA")), 
               merge(data[STRATA == "ZERO" & AGENCY_GEAR_CODE != "JIG"], strata_timeliness, by.x = c("COVERAGE_TYPE", "AGENCY_GEAR_CODE"), by.y = c("COVERAGE_TYPE", "STRATA"), all.x = TRUE))
