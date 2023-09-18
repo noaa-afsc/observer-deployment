26ac8dff98dc66e8beec451c488f70b53f0b62c4
Phil.Ganz <phil.ganz@noaa.gov>
2023-09-18 14:00:50 -0400

    first draft of stratum-specific trip-level variance evaluation

--- evaluate_designs.R
+++ evaluate_designs.R
@@ -53,6 +53,57 @@ for(i in 1:nrow(Scenarios)){
   trip_variance_results <- rbind(trip_variance_results, data.table(Scenarios[i], evaluate_trip_variance(effort, work.data, trips_melt, Scenarios[i, BUDGET], rates, Scenarios[i, Stratification], Scenarios[i, Allocation])[, .(variable = metric, value = cv)]))
 }
 
+trip_variance_strata_results <- data.table()
+effort_wd_join <- "wd_TRIP_ID"
+
+for(i in 1:nrow(Scenarios)){
+  effort         <- get(paste0("pc_effort.", Scenarios[i, Stratification]))[ADP == Scenarios[i, Year]]
+  budget         <- Scenarios[i, BUDGET]
+  stratification <- Scenarios[i, Stratification]
+  allocation     <- Scenarios[i, Allocation]
+  
+  # Rename columns
+  metrics <- trips_melt[, .(TRIP_ID, METRIC = Metric, VALUE = Value)]
+  
+  # Convert optimization metrics to wide format
+  metrics <- dcast(metrics[, .(TRIP_ID, METRIC, VALUE)], TRIP_ID ~ METRIC, value.var = "VALUE")
+  
+  # Get the original monitoring status of trips in the effort object
+  wd <- unique(work.data[, .(TRIP_ID, OBSERVED_FLAG)])
+  colnames(wd)[colnames(wd) == "TRIP_ID"] <- effort_wd_join
+  effort <- wd[effort, on = c(effort_wd_join)]
+  effort <- unique(effort[, c(..effort_wd_join, "ADP", "STRATUM_COL", "OBSERVED_FLAG")])
+  
+  # Join optimization metric values with the most recent year of trips
+  colnames(metrics)[colnames(metrics) == "TRIP_ID"] <- effort_wd_join
+  data <- metrics[effort, on = c(effort_wd_join)]
+  
+  # Total number of trips and expected number of monitored trips per stratum
+  data <- unique(rates[[paste(stratification, allocation, sep = ".")]][BUDGET == budget, .(ADP, STRATUM_COL, N = STRATA_N, n)])[data, on = .(ADP, STRATUM_COL), nomatch=0]
+  
+  # Sum metrics by columns of interest
+  data <- data[, .(chnk_psc = sum(chnk_psc), hlbt_psc = sum(hlbt_psc), discard = sum(discard), crab_psc = sum(crab_psc)), by = c(effort_wd_join, "STRATUM_COL", "OBSERVED_FLAG", "N", "n")]
+  
+  # Calculate trip-level mean and variance of monitored trips
+  mean <- data[, lapply(.SD, function(x) mean(x[OBSERVED_FLAG=="Y"], na.rm = TRUE)), .SDcols = c("chnk_psc", "hlbt_psc", "discard", "crab_psc"), keyby = c("STRATUM_COL", "N", "n")]
+  var <- data[, lapply(.SD, function(x) var(x[OBSERVED_FLAG=="Y"], na.rm = TRUE)), .SDcols = c("chnk_psc", "hlbt_psc", "discard", "crab_psc"), keyby = c("STRATUM_COL", "N", "n")]
+  
+  mean <- melt(mean, id.vars = c("STRATUM_COL", "N", "n"), variable.name = "metric", value.name = "mean")
+  var <- melt(var, id.vars = c("STRATUM_COL", "N", "n"), variable.name = "metric", value.name = "var")
+  
+  data <- merge(mean, var, by = c("STRATUM_COL", "N", "n", "metric"))
+  
+  # Estimate the stratum-level mean and variance  
+  data[, mean := N * mean]
+  data[, var := N^2 * (N-n)/N * 1/n * var]
+  
+  # Calculate CV
+  data[, cv := ifelse(mean > 0, round(sqrt(var) / mean, 4), 0)]
+  
+  # Append results
+  trip_variance_strata_results <- rbind(trip_variance_strata_results, cbind(Scenarios[i], data[, .(STRATUM_COL, variable = metric, value = cv)]))
+}
+
 # * Format results ----
 trip_variance_results <- trip_variance_results[, ":=" (Category = "Trip-Level\nVariance (CV)", 
                                                        BUDGET = recode(as.character(BUDGET), "3500000" = "$3.5M", "4500000" = "$4.5M", "5250000" = "$5.25M"),
@@ -84,7 +135,26 @@ DIFF := (scaled_value - worst) / spread][,
                                          "chnk_psc" = "Chinook PSC"),
       Allocation = recode(Allocation, "STATUS_QUO" = "STATUS\nQUO"))]
 
+# Multiply by -1 for metrics for which larger numbers are worse
+trip_variance_strata_results[, ':=' (label = formatC(value * 100, format = "f", digits = 2), scaled_value = value, Category = "Trip-Level\nVariance (CV)")][
+, scaled_value := -1 * scaled_value][, 
+# Get the maximum and minimum value within the category
+':=' (worst = min(scaled_value), best = max(scaled_value)), by = .(Category)][,
+# Calculate the spread between best and worst
+spread := best - worst][,                                                                                                                                                                                               
+# Find the relative difference
+DIFF := (scaled_value - worst) / spread][, 
+# Recode columns
+':=' (BUDGET = recode(as.character(BUDGET), "3500000" = "$3.5M", "4500000" = "$4.5M", "5250000" = "$5.25M"),
+variable = recode_factor(variable, "crab_psc" = "Crab PSC",
+                                   "discard"  = "Discards",
+                                   "hlbt_psc" = "Halibut PSC",
+                                   "chnk_psc" = "Chinook PSC"),
+Allocation = recode(Allocation, "STATUS_QUO" = "STATUS\nQUO"))]
+
 # Figures ----
+
+# * Main ----
 
 ggplot(scorecard_dt[BUDGET == "$3.5M"], aes(x = Allocation, y = variable, fill = DIFF)) +
   facet_nested(Category ~ BUDGET + Stratification, scales = "free", space = "free", switch = "y", labeller = labeller(
@@ -121,3 +191,41 @@ ggplot(scorecard_dt[BUDGET == "$5.25M"], aes(x = Allocation, y = variable, fill 
   labs(fill = "", y = "Metric")
 
 # ggsave("output_figures/evaluation_5.25.png", width = 8, height = 10, units = "in")
+
+# * Stratum-specific ----
+
+ggplot(trip_variance_strata_results[BUDGET == "$3.5M"], aes(x = Allocation, y = STRATUM_COL, fill = DIFF)) +
+  facet_nested(Stratification ~ BUDGET + variable, scales = "free", space = "free", switch = "y", labeller = labeller(
+    BUDGET = function(x) paste("Budget: ", x),
+    Stratification = function(x) paste("Stratification: ", x))) +
+  geom_tile() +
+  scale_fill_gradient2(low = muted("red", l = 40), high = muted("blue", l = 40), midpoint = 0.5, breaks = c(0, 1), labels = c("Worst", "Best")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), legend.position = "bottom", legend.key.width = unit(1, "in")) +
+  geom_text(aes(label = label), size = 3) +
+  labs(fill = "", y = "Metric")
+
+# ggsave("output_figures/evaluation_strata_3.5.png", width = 8, height = 10, units = "in")
+
+ggplot(trip_variance_strata_results[BUDGET == "$4.5M"], aes(x = Allocation, y = STRATUM_COL, fill = DIFF)) +
+  facet_nested(Stratification ~ BUDGET + variable, scales = "free", space = "free", switch = "y", labeller = labeller(
+    BUDGET = function(x) paste("Budget: ", x),
+    Stratification = function(x) paste("Stratification: ", x))) +
+  geom_tile() +
+  scale_fill_gradient2(low = muted("red", l = 40), high = muted("blue", l = 40), midpoint = 0.5, breaks = c(0, 1), labels = c("Worst", "Best")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), legend.position = "bottom", legend.key.width = unit(1, "in")) +
+  geom_text(aes(label = label), size = 3) +
+  labs(fill = "", y = "Metric")
+
+# ggsave("output_figures/evaluation_strata_4.5.png", width = 8, height = 10, units = "in")
+
+ggplot(trip_variance_strata_results[BUDGET == "$5.25M"], aes(x = Allocation, y = STRATUM_COL, fill = DIFF)) +
+  facet_nested(Stratification ~ BUDGET + variable, scales = "free", space = "free", switch = "y", labeller = labeller(
+    BUDGET = function(x) paste("Budget: ", x),
+    Stratification = function(x) paste("Stratification: ", x))) +
+  geom_tile() +
+  scale_fill_gradient2(low = muted("red", l = 40), high = muted("blue", l = 40), midpoint = 0.5, breaks = c(0, 1), labels = c("Worst", "Best")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), legend.position = "bottom", legend.key.width = unit(1, "in")) +
+  geom_text(aes(label = label), size = 3) +
+  labs(fill = "", y = "Metric")
+
+# ("output_figures/evaluation_strata_5.25.png", width = 8, height = 10, units = "in")
