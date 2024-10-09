23309c325105e7e3f93188d2e70e92d0bfe48c0a
Geoffrey Mayhew <geoff.mayhew@noaa.gov>
2024-10-09 14:32:39 -0700

    Removed commented-out queries that now exist within akro_sql_pull.R

--- get_data.R
+++ get_data.R
@@ -28,61 +28,6 @@ load(paste0("source_data/", paste(ADPyear, ADP_version, "ADP_akro_pull.rdata", s
 
 # Data queries ------------------------------------------------------------
 
-# * PSC ----
-# PSC <- 
-#   dbGetQuery(channel_akro,
-#              paste("SELECT
-#                    year,
-#                    el_report_id as report_id,
-#                    species_group_code, 
-#                    sum(case when psc_total_count is not null then psc_total_count
-#                             when psc_total_catch_weight is not null then psc_total_catch_weight
-#                             else 0 end) as psc_total_catch,
-#                    sum(psc_total_mortality_weight) as psc_total_mortality_weight
-#                    FROM akfish_report.v_cas_psc
-#                    WHERE year >=", ADPyear - 4,"
-#                    AND el_report_id is not null
-#                    GROUP BY year, el_report_id, species_group_code"))
-
-# * PCTC ----
-# pctc <- dbGetQuery(channel_akro, 
-#                    "SELECT DISTINCT llp.current_vessel_id vessel_id, llp.current_vessel_name vessel_name
-#                     FROM akfish.v_llp_groundfish_license llp
-#                     JOIN akfish.pctc_llp_initial_quota_share qs ON substr(llp.license,4,4) = qs.license_number
-#                     WHERE llp.current_vessel_id is not null
-#                     ORDER BY vessel_name")
-
-# * Voluntary full coverage ----
-#   Requests to join must be made prior to October 15  
-# BSAIVoluntary <-
-#   dbGetQuery(channel_akro,
-#              if(ADP_version == "Draft" | Sys.Date() < paste0(ADPyear-1, "-10-15")){
-#                paste0(
-#                  "select distinct 
-#                   ev.vessel_id, ev.begin_date, ev.end_date, v.name as vessel_name, e.name as eligibility, trunc(ev.last_modified_date) as last_modified_date
-#                   from akfish.eligible_vessel ev
-#                   join akfish.eligibility e on e.id = ev.eligibility_id
-#                   join akfish_report.vessel v on v.vessel_id = ev.vessel_id
-#                   where e.name = 'CV FULL COVERAGE'
-#                   and v.end_date is null
-#                   and v.expire_date is null
-#                   and extract(year from ev.begin_date) < ", ADPyear,"
-#                   and (ev.end_date is null or extract(year from ev.end_date) >= ", ADPyear - 1,")
-#                   order by v.name")} else
-#                    paste0(
-#                      "select distinct 
-#                       ev.vessel_id, ev.begin_date, ev.end_date, v.name as vessel_name, e.name as eligibility, trunc(ev.last_modified_date) as last_modified_date
-#                       from akfish.eligible_vessel ev
-#                       join akfish.eligibility e on e.id = ev.eligibility_id
-#                       join akfish_report.vessel v on v.vessel_id = ev.vessel_id
-#                       where e.name = 'CV FULL COVERAGE'
-#                       and v.end_date is null
-#                       and v.expire_date is null
-#                       and extract(year from ev.begin_date) < ", ADPyear + 1,"
-#                       and (ev.end_date is null or extract(year from ev.end_date) >= ", ADPyear,")
-#                       order by v.name")
-#              )
-
 # * Partial Coverage CPs ----
 #   Requests to join must be made prior to July 1  
 #   F/V Trident (VESSEL_ID == 662) doesn't have an FFP and operates solely in state waters 
@@ -186,20 +131,6 @@ work.data <- mutate(work.data, VESSEL_ID = as.character(VESSEL_ID))
 work.data <- mutate(work.data, TENDER = toupper(TENDER))
 
 # Defer to database dates for all trips in valhalla with dates that don't match the database
-# up_dates <- setDT(dbGetQuery(channel_akro, paste0("select distinct 
-#                                                   cr.el_report_id as report_id,
-#                                                   td.calendar_date as db_trip_target_date,
-#                                                   cr.el_report_date as db_landing_date,
-#                                                   g.gear_code as db_agency_gear_code
-#                                                   FROM akfish_report.transaction_fact tf
-#                                                   JOIN akfish_report.v_current_keys K ON tf.TRANSACTION_FACT_LOG_PK = k.transaction_fact_log_pk
-#                                                   AND tf.PARTITION_KEY_PK = k.partition_key_pk
-#                                                   JOIN akfish_report.catch_report cr on cr.catch_report_pk= tf.catch_report_pk
-#                                                   JOIN akfish_report.calendar_date td on td.calendar_date_pk = tf.trip_target_date_pk
-#                                                   JOIN akfish_report.gear g on g.gear_pk = tf.gear_pk
-#                                                   WHERE k.year >=", ADPyear - 12,"
-#                                                   AND cr.el_report_id is not null")))
-
 up_dates[, ':='(DB_TRIP_TARGET_DATE = as.Date(DB_TRIP_TARGET_DATE), DB_LANDING_DATE = as.Date(DB_LANDING_DATE))]
 
 # View the changes to be made to dates
